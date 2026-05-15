e544f10a1ab5a01708edb71915cb4a375ad0910c
KNOOP <knoopnu@knoopnu-334.local>
2026-05-15 15:47:42 +0800

    chore: bump version to 8.0.0

--- custom_components/claw_assistant/const.py
+++ custom_components/claw_assistant/const.py
@@ -1,5 +1,5 @@
 DOMAIN = "claw_assistant"
-VERSION = "7.9.0"
+VERSION = "8.0.0"
 
 CONF_PRIMARY_AGENT = "primary_agent"
 CONF_FALLBACK_AGENT = "fallback_agent"
@@ -10,12 +10,15 @@ CONF_ENABLE_AI_SUMMARY = "enable_ai_summary"
 CONF_ENABLE_WEB_SEARCH = "enable_web_search"
 CONF_ENABLE_STREAMING_EFFECT = "enable_streaming_effect"
 CONF_CONTINUOUS_CONVERSATION = "continuous_conversation"
+CONF_ENABLE_SOUND_NOTIFICATIONS = "enable_sound_notifications"
 CONF_ENABLE_CONTEXT_STATUS_BAR = "enable_context_status_bar"
 CONF_ENABLE_FILE_UPLOAD = "enable_file_upload"
 CONF_ENABLE_RICH_MARKDOWN = "enable_rich_markdown"
 CONF_ENABLE_SIDEBAR_DOCK = "enable_sidebar_dock"
 CONF_ENABLE_TOOL_PROGRESS = "enable_tool_progress"
 CONF_MAX_TOOL_REPEAT = "max_tool_repeat"
+CONF_IDENTICAL_CALL_WARN = "identical_call_warn"
+CONF_IDENTICAL_CALL_STOP = "identical_call_stop"
 CONF_PIPELINE_TIMEOUT = "pipeline_timeout"
 
 CONVERSATION_MODE_NO_NAME = "no_name"
@@ -27,4 +30,6 @@ DEFAULT_PRIMARY_AGENT = ""
 DEFAULT_FALLBACK_AGENT = ""
 DEFAULT_SECONDARY_FALLBACK_AGENT = None
 DEFAULT_MAX_TOOL_REPEAT = 15
+DEFAULT_IDENTICAL_CALL_WARN = 10
+DEFAULT_IDENTICAL_CALL_STOP = 20
 DEFAULT_PIPELINE_TIMEOUT = 900
